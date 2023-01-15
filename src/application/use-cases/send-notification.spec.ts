386168c75ac5d3255193a586b66c70b6b6d7f6db
rogen <rogendouglas@hotmail.com>
2023-01-15 17:28:49 -0400

    test: create database in memory to test

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,26 +1,23 @@
 import { randomUUID } from 'crypto';
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationRepository } from '../../../test/repositories/in-memory-notification-repository';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationRepository = {
-  // eslint-disable-next-line
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('Should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(notificationRepository);
+    const inMemoryNotificationRepository = new InMemoryNotificationRepository();
+    const sendNotification = new SendNotification(
+      inMemoryNotificationRepository,
+    );
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       recipientId: randomUUID(),
       content: 'This is a notification',
       category: 'social',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(inMemoryNotificationRepository.notifications).toHaveLength(1);
+    expect(inMemoryNotificationRepository.notifications[0]).toEqual(
+      notification,
+    );
   });
 });
