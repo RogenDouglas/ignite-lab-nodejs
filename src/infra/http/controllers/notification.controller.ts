a388ef6c69f0f4b63aa963af6b6159e2e756ebeb
rogen <rogendouglas@hotmail.com>
2023-01-15 19:33:20 -0400

    feat: create method to persist the notification

---
+++ src/infra/http/controllers/notification.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { CreateNotificationDTO } from '../dtos/create-notificationDTO';
+
+@Controller('notifications')
+export class NotificationController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() createNotificationDTO: CreateNotificationDTO) {
+    const { recipientId, content, category } = createNotificationDTO;
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return {
+      notification,
+    };
+  }
+}
