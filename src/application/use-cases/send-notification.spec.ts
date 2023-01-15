3a186cfd952cb18ff3c01c58fe4b869b458321c0
rogen <rogendouglas@hotmail.com>
2023-01-15 17:10:43 -0400

    test: add notification repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,16 +1,26 @@
 import { randomUUID } from 'crypto';
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  // eslint-disable-next-line
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('Should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: randomUUID(),
       content: 'This is a notification',
       category: 'social',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
