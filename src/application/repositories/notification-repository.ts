f3dbff684bdb5490d78bc0ae08f08465bbb68388
rogen <rogendouglas@hotmail.com>
2023-01-16 20:53:26 -0400

    feat: create count recipient notifications method

--- src/application/repositories/notification-repository.ts
+++ src/application/repositories/notification-repository.ts
@@ -4,4 +4,7 @@ export abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract findById(notificationId: string): Promise<Notification | null>;
   abstract save(notification: Notification): Promise<void>;
+  abstract countNotificationsByRecipientId(
+    recipientId: string,
+  ): Promise<number>;
 }
