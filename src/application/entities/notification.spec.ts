c1ff718c9b6b20c49c3917700b1abb087d1e64b3
rogen <rogendouglas@hotmail.com>
2023-01-16 22:31:47 -0400

    refactor: change notification entity

--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -1,14 +1,8 @@
-import { randomUUID } from 'crypto';
-import { Content } from './content';
-import { Notification } from './notification';
+import { makeNotification } from '@test/factories/notification-factory';
 
 describe('Notification', () => {
   it('should be able to create a notification', () => {
-    const notification = new Notification({
-      recipientId: randomUUID(),
-      content: new Content('Você recebeu uma mensagem.'),
-      category: 'social',
-    });
+    const notification = makeNotification();
 
     expect(notification).toBeTruthy();
   });
