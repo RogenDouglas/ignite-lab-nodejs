5efe5b4ee5ee4ef4215d93432b9fcf82825ad154
rogen <rogendouglas@hotmail.com>
2023-01-16 22:29:25 -0400

    feat: create use cases get, read and unread

---
+++ src/application/use-cases/get-recipient-notifications.ts
@@ -0,0 +1,31 @@
+import { Notification } from '@application/entities/notification';
+import { Injectable } from '@nestjs/common';
+import { NotificationRepository } from '../repositories/notification-repository';
+
+interface GetRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationsResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: GetRecipientNotificationsRequest,
+  ): Promise<GetRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const notifications =
+      await this.notificationRepository.findNotificationsByRecipientId(
+        recipientId,
+      );
+
+    return {
+      notifications,
+    };
+  }
+}
